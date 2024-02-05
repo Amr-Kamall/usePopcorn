633a24d92e50e56139c621245e4de8a821c14d06
amr <amr442028@gmail.com>
2024-02-05 18:50:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import Movies from "./Movies";
-import MovieDetails from "./MovieDetails";
 import WatchedSummary from "./WatchedSummary";
 
 // const tempWatchedData = [
@@ -35,7 +34,7 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
-    return JSON.parse(storedValue);
+    return JSON.parse(storedValue) || [];
   });
 
   const [error, setError] = useState("");
